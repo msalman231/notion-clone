56d9890d6c5f7fe285fc689d1b496174884834f3
msalman231 <iff.salman366@gmail.com>
2026-02-25 16:39:22 +0530

    key is fixed

--- firebase.ts
+++ firebase.ts
@@ -6,16 +6,16 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDmM_cwXumBjdOUv1Q8rqwZdvNiOBYr0v8",
+  apiKey: "API_KEY",
   authDomain: "notion-clone-d328f.firebaseapp.com",
   projectId: "notion-clone-d328f",
   storageBucket: "notion-clone-d328f.firebasestorage.app",
   messagingSenderId: "1069353757162",
-  appId: "1:1069353757162:web:d5c7847dd798a2cfff2a93"
+  appId: "1:1069353757162:web:d5c7847dd798a2cfff2a93",
 };
 
 // Initialize Firebase
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp(); 
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 
-export {db};
+export { db };
