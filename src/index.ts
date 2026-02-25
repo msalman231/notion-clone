fb1c4c4071ba8619100d185a13bd45b0ff0726ec
msalman231 <iff.salman366@gmail.com>
2026-02-25 16:31:56 +0530

    backend-notion-clone

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,119 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import OpenAI from 'openai';
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+type Bindings = {
+	OPEN_AI_KEY: string;
+	AI: Ai;
+};
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.use(
+	'/*',
+	cors({
+		origin: '*',
+		allowHeaders: ['X-Custom-Header', 'Upgrade_insecure-Requests', 'Content-Type'],
+		allowMethods: ['POST', 'GET', 'OPTIONS', 'PUT'],
+		exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+		maxAge: 600,
+		credentials: true,
+	}),
+);
+app.get('/', (c) => {
+	return c.text('AI Worker is running 🚀');
+});
+app.post('/translateDocument', async (c) => {
+	const { documentData, targetLang } = await c.req.json();
+
+	const summaryResponse = await c.env.AI.run('@cf/facebook/bart-large-cnn', {
+		input_text: documentData,
+		max_length: 1000,
+	});
+
+	const res = await c.env.AI.run('@cf/meta/m2m100-1.2b', {
+		text: summaryResponse.summary,
+		source_lang: 'english',
+		target_lang: targetLang,
+	});
+
+	return new Response(JSON.stringify(res));
+});
+
+// app.post('/chatToDocument', async (c) => {
+// 	const openai = new OpenAI({
+// 		apiKey: c.env.OPEN_AI_KEY,
+// 	});
+
+// 	const { documentData, question } = await c.req.json();
+
+// 	const documentText = typeof documentData === 'string' ? documentData : JSON.stringify(documentData, null, 2);
+
+// 	const chatCompletion = await openai.chat.completions.create({
+// 		messages: [
+// 			{
+// 				role: 'system',
+// 				content:
+// 					'You are an AI assistant that answers questions strictly based on the provided document. ' +
+// 					'If the answer is not present in the document, say "The document does not contain this information."',
+// 			},
+// 			{
+// 				role: 'system',
+// 				content: `DOCUMENT CONTENT:\n${documentText}`,
+// 			},
+// 			{
+// 				role: 'user',
+// 				content: question,
+// 			},
+// 		],
+// 		model: 'gpt-4o',
+// 		temperature: 0.5,
+// 	});
+// 	const res = chatCompletion.choices[0].message.content;
+
+// 	return c.json({ message: res });
+// });
+
+app.post('/chatToDocument', async (c) => {
+	try {
+		const openai = new OpenAI({
+			apiKey: c.env.OPEN_AI_KEY,
+		});
+
+		const { documentData, question } = await c.req.json();
+
+		if (!documentData || !question) {
+			return c.json({ err: 'Invald payload' }, 400);
+		}
+
+		const documentText = typeof documentData === 'string' ? documentData : JSON.stringify(documentData, null, 2);
+
+		const res = await openai.responses.create({
+			model: 'gpt-4o',
+			input: [
+				{
+					role: 'system',
+					content:
+						'You are an AI assistant that answers questions strictly based on the provided document.' +
+						'If the answer is not present in the document, say "The document does not contain this information."',
+				},
+				{
+					role: 'system',
+					content: `Document Content: \n${documentText}`,
+				},
+				{
+					role: 'user',
+					content: question,
+				},
+			],
+			temperature: 0.5,
+		});
+		return c.json({
+			mes: res.output_text,
+		});
+	} catch (err: any) {
+		console.error('chatToDocument error: ', err);
+		return c.json({ err: err?.mes ?? 'Internal Server Error' }, 500);
+	}
+});
+
+export default app;
